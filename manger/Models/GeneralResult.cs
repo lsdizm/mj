60d19341f5b062d63c61f9622b6cc97170d9e39d
park byoung chul <ezcaretech.park.b.c@DESKTOP-CAV95B9>
2022-07-05 08:03:01 +0900

    add-new-controller

---
+++ manger/Models/GeneralResult.cs
@@ -0,0 +1,11 @@
+namespace manger.Models;
+
+public class GeneralResult
+{
+    public bool IsCompleted {get; set;}
+    public string ResultText {get; set;}
+
+    public GeneralResult()
+    {
+    }
+}
